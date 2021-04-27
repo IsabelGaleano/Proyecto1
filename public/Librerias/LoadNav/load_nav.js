b8594cc4a4af1ccd99eb905d45a4fedb93a37d74
Isabel Galeano <isagaleano9@gmail.com>
2021-04-27 00:25:35 -0600

    Add imágen y nombre del usuario logged-in en los landing pages

---
+++ public/Librerias/LoadNav/load_nav.js
@@ -0,0 +1,15 @@
+const LOGGED_USER_EMAIL = localStorage.getItem('correo');
+
+const element_img = document.getElementById('logged_user_img');
+const element_name = document.getElementById('logged_user_name');
+
+fetch('http://localhost:5000/usuarios/buscar', {
+  method: 'POST',
+  body: JSON.stringify({ correo: LOGGED_USER_EMAIL }),
+  headers: { 'Content-Type': 'application/json' },
+})
+  .then(res => res.json())
+  .then(data => {
+    element_img.src = './../../uploads/' + data[0].imagen_usuario;
+    element_name.innerText = data[0].nombre;
+  });
