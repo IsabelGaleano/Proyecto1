1dbc0430b805049245f3ace1e1c4366a910022aa
Isabel Galeano <isagaleano9@gmail.com>
2021-04-28 23:54:36 -0600

    Fix multer a base64 para imágenes categoria de servicios

--- public/Administrador/actualizarCategoriaServicio/actualizar_categoria_servicio.js
+++ public/Administrador/actualizarCategoriaServicio/actualizar_categoria_servicio.js
@@ -1,71 +1,59 @@
 document.querySelector('#revisarCategoria').addEventListener('click', e => {
-    let revisar = document.getElementById("revisarCategoria");
-    let error = revisarForm();
-    if (!error) {
-        actualizarCategoria();
-        revisar.setAttribute("href", "../listadoCategoriaServicios/listado_categorias_servicios.html")
-        
-
-    } else {
-        Swal.fire({
-            title: 'Error!',
-            text: 'Campos vacíos',
-            icon: 'error',
-            confirmButtonText: 'Aceptar'
-        })
-
-    }
-
-
-})
+  let revisar = document.getElementById('revisarCategoria');
+  let error = revisarForm();
+  if (!error) {
+    actualizarCategoria();
+    revisar.setAttribute(
+      'href',
+      '../listadoCategoriaServicios/listado_categorias_servicios.html'
+    );
+  } else {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Campos vacíos',
+      icon: 'error',
+      confirmButtonText: 'Aceptar',
+    });
+  }
+});
 
 const cargarCategoria = () => {
-    let nombre = localStorage.getItem('data-nombre-categoria');
-    var datos = {
-      nombre: nombre
-    }
-  
-    fetch("http://localhost:5000/categorias_servicios/buscar", {
-      method: 'POST',
-      body: JSON.stringify(datos),
-      headers: { 'Content-Type': 'application/json' }
+  let nombre = localStorage.getItem('data-nombre-categoria');
+  var datos = {
+    nombre: nombre,
+  };
+
+  fetch('http://localhost:5000/categorias_servicios/buscar', {
+    method: 'POST',
+    body: JSON.stringify(datos),
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then(response => {
+      return response.json();
     })
-      .then(
-        response => {
-          return response.json();
-        }
-      )
-      .then(
-        json => {
-  
-          let imagen = document.getElementById('img_categoria_servicio');
-          for (let i = 0; json.length > i; i++) {
-            document.getElementById('nombre').value = json[i].nombre;
-            document.getElementById('descripcion').value = json[i].descripcion;
-            imagen.setAttribute('src', `./../../uploads/${json[i].imagen}`);
-          }
-  
-  
-        }
-      )
-  }
-  
+    .then(json => {
+      let imagen = document.getElementById('img_categoria_servicio');
+      for (let i = 0; json.length > i; i++) {
+        document.getElementById('nombre').value = json[i].nombre;
+        document.getElementById('descripcion').value = json[i].descripcion;
+        document.getElementById('img_categoria_servicio').src = json[i]?.imagen
+          ? json[i].imagen
+          : '../../img/agregarImg.jpg';
+      }
+    });
+};
 
 const actualizarCategoria = () => {
   var datos = {
     nombre: document.getElementById('nombre').value,
     descripcion: document.getElementById('descripcion').value,
-    imagen: document.getElementById('imagen_nueva').files[0],
+    imagen: document.getElementById('img_categoria_servicio').src,
   };
-
-  const formData = new FormData();
-  for (let key in datos) {
-    formData.append(key, datos[key]);
-  }
 
   fetch('http://localhost:5000/categorias_servicios/actualizar', {
     method: 'PUT',
-    body: formData,
+    body: JSON.stringify(datos),
+    headers: { 'Content-Type': 'application/json' },
   })
     .then(response => {
       return response.json();
@@ -75,29 +63,15 @@ const actualizarCategoria = () => {
     });
 };
 
-const loadImagePreview = fileInput => {
-  var datos = {
-    imagen: fileInput.files[0],
-  };
+const imgPreview = async (e) => {
+  try {
+      const img = e.files[0];
 
-  const formData = new FormData();
-  for (let key in datos) {
-    formData.append(key, datos[key]);
+      if (img) {
+          const base64Img = await toBase64(img);
+          document.getElementById('img_categoria_servicio').src = base64Img;
+      }
+  } catch (e) {
+      throw e;
   }
-
-  fetch('http://localhost:5000/upload_preview', {
-    method: 'POST',
-    body: formData,
-  })
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      const filename = data.filename;
-      let imgElement = document.getElementById('img_categoria_servicio');
-      imgElement.src = `./../../uploads_preview/${filename}`;
-    })
-    .catch(err => {
-      response.json({ message: err });
-    });
 };
